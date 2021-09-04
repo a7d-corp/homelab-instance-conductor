9940bff83504562b9f0ef52a1f15914c3e166f2d
Simon Weald <glitchcrab-github@simonweald.com>
2021-09-04 17:00:58 +0100

    rename instance

--- terraform/zz_locals.tf
+++ terraform/zz_locals.tf
@@ -4,6 +4,6 @@ locals {
   snippet_dir        = "snippets"
   primary_ip_gateway = cidrhost(var.net0_network_cidr, 1)
   ip_offset          = 4
-  instance_name      = "internal-infra"
+  instance_name      = "infra-conductor"
   pm_host_address    = data.vault_generic_secret.terraform_generic.data["host"]
 }
