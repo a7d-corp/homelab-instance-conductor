77248de6cea4fe9c3e99489da3941c50984d977b
Simon Weald <glitchcrab-github@simonweald.com>
2021-09-04 12:56:01 +0100

    initial commit

---
+++ terraform/zz_locals.tf
@@ -0,0 +1,9 @@
+locals {
+  vmid               = 201
+  snippet_root_dir   = "/mnt/pve/cloudinit"
+  snippet_dir        = "snippets"
+  primary_ip_gateway = cidrhost(var.net0_network_cidr, 1)
+  ip_offset          = 4
+  instance_name      = "internal-infra"
+  pm_host_address    = data.vault_generic_secret.terraform_generic.data["host"]
+}
---
+++ terraform/zz_remote_state.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    force_path_style            = true
+    key                         = "terraform.tfstate"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+  }
+}
