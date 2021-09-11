23d25bbcc476ebd09dfb004f059b26408fa3846a
Simon Weald <glitchcrab-github@simonweald.com>
2021-09-11 22:26:12 +0100

    rename instance

--- terraform/zz_remote_state.tf
+++ terraform/zz_remote_state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket                      = "homelab-infra-instance"
+    bucket                      = "homelab-instance-conductor"
     force_path_style            = true
     key                         = "terraform.tfstate"
     skip_credentials_validation = true
